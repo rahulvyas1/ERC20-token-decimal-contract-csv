8fbebc113908d45c795f4f5052cf5c69673d7b2c
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-17 03:20:29 +0530

    final commit

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ REQUEST_URL = "getTokenInfo/"
 token_address = "0xb8c77482e45f1f44de1745f52c74426c631bdd52"
 API_URL = "?apiKey=freekey"
 
-data = requests.get(URL+REQUEST_URL+token_addres+API_URL)
-
-print(data.json())
 addresses = []
 decimals = []
 names = []
+
+
 #Read from the CSV file
 with open("/Users/rahulvyas/token_contract.csv") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ' ',quotechar='|')
@@ -27,11 +26,11 @@ with open("/Users/rahulvyas/token_contract.csv") as csv_file:
 
 #####WARNING#####
 ##ETHPLORER API allows 1 call ever 2 seconds for testing##
-
 with open('/Users/rahulvyas/coins_data.txt', 'w') as txtFile:
     for address in addresses:
         data = requests.get(URL+REQUEST_URL+address+API_URL)
         #wait 2 seconds after 1 request
-        txtFile.write(data.json()['symbol'] + ": " + address + " ," + str(data.json()['decimals']) + "\n" )
-        print(data.json()['symbol'] + ": " + address + " ," + str(data.json()['decimals']))
+        txtFile.write(data.json()['name'] + ": " + data.json()['symbol'] + ": " + address + " ," + str(data.json()['decimals']) + "\n" )
+        print(data.json()['name'] + ": " + data.json()['symbol'] + ": " + address + " ," + str(data.json()['decimals']))
         time.sleep(1)
+
