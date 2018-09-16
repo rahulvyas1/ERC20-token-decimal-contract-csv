a4e141eb6092556933dc954fd364925554651bdc
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-17 02:14:50 +0530

    fixed the read code

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ API_URL = "?apiKey=freekey"
 data = requests.get(URL+REQUEST_URL+token_addres+API_URL)
 
 print(data.json())
-
+addresses = []
 
 #Read from the CSV file
 with open("/Users/rahulvyas/token_contract.csv") as csv_file:
-    ...:     csv_reader = csv.reader(csv_file, delimiter = ' ',quotechar='|')
-    ...:     for row in csv_reader:
-    ...:         if len(row) <= 1:
-    ...:             continue
-    ...:         print(type(row))
-    ...:         print(row[1].replace("'", "")[:-1])
+        csv_reader = csv.reader(csv_file, delimiter = ' ',quotechar='|')
+        for row in csv_reader:
+            if len(row) <= 1:
+                continue
+            print(type(row))
+            print(row[1].replace("'", "")[:-1])
+            addresses.append(row[1].replace("'", "")[:-1])
+
