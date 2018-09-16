79ec92b62bd23c31ffa15b9226578520f6b16e58
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-17 01:52:44 +0530

    csv reading test

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import csv
+
+URL = "http://api.ethplorer.io/"
+REQUEST_URL = "getTokenInfo/"
+token_address = "0xb8c77482e45f1f44de1745f52c74426c631bdd52"
+API_URL = "?apiKey=freekey"
+
+data = requests.get(URL+REQUEST_URL+token_addres+API_URL)
+
+print(data.json())
+
+
+#Read from the CSV file
+with open("/Users/rahulvyas/token_contract.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ' ',quotechar='|')
+        for row in csv_reader:
+            print(row)
