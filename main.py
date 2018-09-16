c4807fb7af093907c8739d6aedebc2ee7591b6cf
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-17 02:51:21 +0530

    retrival of all coin data working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-
+import time
 URL = "http://api.ethplorer.io/"
 REQUEST_URL = "getTokenInfo/"
 token_address = "0xb8c77482e45f1f44de1745f52c74426c631bdd52"
@@ -10,7 +10,8 @@ data = requests.get(URL+REQUEST_URL+token_addres+API_URL)
 
 print(data.json())
 addresses = []
-
+decimals = []
+names = []
 #Read from the CSV file
 with open("/Users/rahulvyas/token_contract.csv") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ' ',quotechar='|')
@@ -21,3 +22,17 @@ with open("/Users/rahulvyas/token_contract.csv") as csv_file:
             print(row[1].replace("'", "")[:-1])
             addresses.append(row[1].replace("'", "")[:-1])
 
+
+#Pulling data & Writing to CSV 
+
+#####WARNING#####
+##ETHPLORER API allows 1 call ever 2 seconds for testing##
+
+with open('/Users/rahulvyas/coins_data.txt', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for address in addresses:
+        data = requests.get(URL+REQUEST_URL+address+API_URL)
+        csvwriter.writerow(data.json()['symbol'] + ": " + address + " ," + data.json()['decimals'])
+        #wait 2 seconds after 1 request
+        print(data.json()['symbol'] + ": " + address + " ," + data.json()['decimals'] )
+        time.sleep(1)
